06feb2444d35d02e8cd097f8e3f66fa5fc79bb21
ljlim <limliangjin@gmail.com>
2022-09-19 22:17:25 +0800

    Add iterations

--- js/l_system.js
+++ js/l_system.js
@@ -17,7 +17,9 @@ var options = {
   currentCharId: 0,
   canvas: null,
   ruleFunction: null,
-  lRate: 1000.0
+  lRate: 1000.0,
+  iteration: 0,
+  maxIteration: 3
 }
 
 // interpret an L-system
@@ -40,10 +42,16 @@ function lindenmayer(s) {
   return outputstring; // send out the modified string
 }
 
+function generateString(){
+  for (let i = 0; i < options.nIterations[options.iteration]; ++i){
+    options.generatedString = lindenmayer(options.generatedString);
+  }
+}
+
 // TREE Rule --------------------------------------------------------
 function setTreeRule(){
-  options.drawLength = 5;
-  options.nIterations = 6;
+  options.drawLength = [20, 10, 5, 3];
+  options.nIterations = [4, 5, 6, 7];
   options.turnAngle = 25;
   options.initial = 'X';
   options.rules[0] = ['X', 'F+[[X]-X]-F[-FX]+X'];
@@ -51,9 +59,7 @@ function setTreeRule(){
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = canvas.width / 2;
@@ -64,18 +70,16 @@ function setTreeRule(){
 
 // DRAGON Rule --------------------------------------------------------
 function setDragonRule(){
-  options.nIterations = 12;
+  options.nIterations = [6, 8, 10, 12];
   options.turnAngle = 90;
-  options.drawLength = 8;
+  options.drawLength = [64, 32, 16, 8];
   options.initial = 'F';
   options.rules[0] = ['F', 'F+G'];
   options.rules[1] = ['G', 'F-G'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = canvas.width / 2;
@@ -86,41 +90,36 @@ function setDragonRule(){
 
 // Sierpinski Triangle Rule --------------------------------------------------------
 function setSierpinskiTriangleRule(){
-  options.nIterations = 7;
+  options.nIterations = [4, 5, 6, 7];
   options.turnAngle = 60;
-  options.drawLength = 3;
+  options.drawLength = [24, 12, 6, 3];
   options.initial = 'F-G-G';
   options.rules[0] = ['F', 'G-F-G'];
   options.rules[1] = ['G', 'F+G+F'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = canvas.width / 3;
   options.state.y = 3 * canvas.height / 4;
   options.state.currentAngle = 0;
-  options.drawFrequency = 10;
   options.state.currentAngle = 0;
   options.title = 'Sierpinski Triangle';
 }
 
 // KochKnot Rule --------------------------------------------------------
 function setKochIslandRule(){
-  options.nIterations = 4;
+  options.nIterations = [1, 2, 3, 4];
   options.turnAngle = 90;
-  options.drawLength = 10;
+  options.drawLength = [80, 40, 20, 10];
   options.initial = 'F-F-F-F';
   options.rules[0] = ['F', 'FF-F+F-F-FF'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = canvas.width / 2;
@@ -132,42 +131,37 @@ function setKochIslandRule(){
 
 // Gosper Rule --------------------------------------------------------
 function setGosperRule(){
-  options.nIterations = 4;
+  options.nIterations = [2, 3, 4, 5];
   options.turnAngle = 60;
-  options.drawLength = 10;
+  options.drawLength = [40, 20, 10, 4];
   options.initial = 'F';
   options.rules[0] = ['F', 'F+G++G-F--FF-G+'];
   options.rules[1] = ['G', '-F+GG++G+F--F-G'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = canvas.width / 2;
   options.state.y = canvas.height / 3;
   options.state.currentAngle = 0;
-  options.drawFrequency = 20;
   options.state.currentAngle = -25;
   options.title = 'Gosper';
 }
 
 // Binary fractal Rule --------------------------------------------------------
 function setBinaryFractalRule(){
-  options.nIterations = 8;
+  options.nIterations = [5, 6, 7, 8];
   options.turnAngle = 45;
-  options.drawLength = 3;
+  options.drawLength = [24, 12, 6, 3];
   options.initial = 'G';
   options.rules[0] = ['F', 'FF'];
   options.rules[1] = ['G', 'F[+G]-G'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = canvas.width / 2;
@@ -181,17 +175,15 @@ function setBinaryFractalRule(){
 
 //Cantor Set Rule --------------------------------------------------------
 function setCantorSetRule(){
-  options.nIterations = 5;
+  options.nIterations = [2, 3, 4, 5];
   options.turnAngle = 90;
-  options.drawLength = 3;
+  options.drawLength = [24, 12, 6, 3];
   options.initial = 'F';
   options.rules[0] = ['F', 'F+F-F-F+F'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = canvas.width / 2;
@@ -205,18 +197,16 @@ function setCantorSetRule(){
 
 //Taurus Rule --------------------------------------------------------
 function setTaurusRule(){
-  options.nIterations = 12;
+  options.nIterations = [6, 8, 10, 12];
   options.turnAngle = 90;
-  options.drawLength = 5;
+  options.drawLength = [40, 20, 10, 5];
   options.initial = 'F';
   options.rules[0] = ['X', 'F+F-'];
   options.rules[1] = ['F', 'X+X-'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = 0.45 * canvas.width;
@@ -229,18 +219,17 @@ function setTaurusRule(){
 
 //Quadratic Gosper Rule --------------------------------------------------------
 function setQuadraticGosperRule(){
-  options.nIterations = 3;
+  options.maxIteration = 2;
+  options.nIterations = [1, 2, 3];
   options.turnAngle = 90;
-  options.drawLength = 5;
+  options.drawLength = [16, 8, 4];
   options.initial = '-YF';
   options.rules[0] = ['X', 'XFX-YF-YF+FX+FX-YF-YFFX+YF+FXFXYF-FX+YF+FXFX+YF-FXYF-YF-FX+FX+YFYF-'];
   options.rules[1] = ['Y', '+FXFX-YF-YF+FX+FXYF+FX-YFYF-FX-YF+FXYFYF-FX-YFFX+FX+YF-YF-FX+FX+YFY'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = 0.65 * canvas.width;
@@ -254,18 +243,17 @@ function setQuadraticGosperRule(){
 
 //Square Sierpinski Rule --------------------------------------------------------
 function setSquareSierpinskiRule(){
-  options.nIterations = 5;
+  options.maxIteration = 3;
+  options.nIterations = [2, 3, 4, 5];
   options.turnAngle = 90;
-  options.drawLength = 5;
+  options.drawLength = [40, 20, 10, 5];
   options.initial = 'F+XF+F+XF';
   options.rules[0] = ['X', 'XF-F+F-XF+F+XF-F+F-X'];
   //options.rules[1] = ['Y', '+FXFX-YF-YF+FX+FXYF+FX-YFYF-FX-YF+FXYFYF-FX-YFFX+FX+YF-YF-FX+FX+YFY'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = 0.5 * canvas.width;
@@ -279,18 +267,17 @@ function setSquareSierpinskiRule(){
 
 //Crystal Rule --------------------------------------------------------
 function setCrystalRule(){
-  options.nIterations = 5;
+  options.maxIteration = 3;
+  options.nIterations = [2, 3, 4, 5];
   options.turnAngle = 90;
-  options.drawLength = 2;
+  options.drawLength = [16, 8, 4, 2];
   options.initial = 'F+F+F+F';
   options.rules[0] = ['F', 'FF+F++F+F'];
   //options.rules[1] = ['Y', '+FXFX-YF-YF+FX+FXYF+FX-YFYF-FX-YF+FXYFYF-FX-YFFX+FX+YF-YF-FX+FX+YFY'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = 0.6 * canvas.width;
@@ -304,18 +291,17 @@ function setCrystalRule(){
 
 //Pentaplexity Rule --------------------------------------------------------
 function setPentaplexityRule(){
-  options.nIterations = 4;
+  options.maxIteration = 3;
+  options.nIterations = [1, 2, 3, 4];
   options.turnAngle = 36;
-  options.drawLength = 8;
+  options.drawLength = [64, 32, 16, 8];
   options.initial = 'F++F++F++F++F';
   options.rules[0] = ['F', 'F++F++F|F-F++F'];
   //options.rules[1] = ['Y', '+FXFX-YF-YF+FX+FXYF+FX-YFYF-FX-YF+FXYFYF-FX-YFFX+FX+YF-YF-FX+FX+YFY'];
   options.currentCharId = 0;
   options.generatedString = options.initial;
 
-  for (let i = 0; i < options.nIterations; ++i){
-    options.generatedString = lindenmayer(options.generatedString);
-  }
+  generateString();
 
   options.savedState = [];
   options.state.x = 0.6 * canvas.width;
@@ -328,8 +314,8 @@ function setPentaplexityRule(){
 }
 
 var RULES = [
-  setGosperRule,
   setKochIslandRule,
+  setGosperRule,
   setSierpinskiTriangleRule,
   setDragonRule,
   setTreeRule,
@@ -353,6 +339,18 @@ var initialize = ()=> {
 
 var gui = null;
 var regenButton = null;
+var iterationButton = null;
+
+
+function increaseIteration(){
+  options.iteration++;
+  if (options.iteration > options.maxIteration) options.iteration = 0;
+  options.ruleFunction = RULES[currentRuleId];
+  options.ruleFunction();
+  background('#222');
+  drawTitle();
+  drawSubTitle();
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -371,6 +369,14 @@ function setup() {
     textSize: 40
   });
   regenButton.onPress = initialize;
+
+  iterationButton = createButton('+', windowWidth / 2 - 120, 0.9 * windowHeight, 80, 80);
+  iterationButton.setStyle({
+    fillBg: color("#FFFFFF"),
+    rounding: 10,
+    textSize: 40
+  });
+  iterationButton.onPress = increaseIteration;
 }
 
 function windowResized() {
@@ -381,7 +387,8 @@ var ca = 0;
 
 function draw() {
   drawGui();
-  for (let i = 0; i < options.drawFrequency; ++i) {
+  let drawFrequency = options.generatedString.length / 100;
+  for (let i = 0; i < drawFrequency; ++i) {
     // draw the current character in the string:
     drawIt(options.generatedString[options.currentCharId]);
     // increment the point for where we're reading the string.
@@ -394,7 +401,7 @@ function draw() {
     stroke(c);
     ca++;
     if (options.currentCharId > options.generatedString.length-1) {
-      options.drawFrequency = 0;
+      drawFrequency = 0;
     }
   }
 }
@@ -404,8 +411,9 @@ function drawIt(k) {
 
   if (k=='F' || k=='G') { // draw forward
     // polar to cartesian based on step and currentangle:
-    let x1 = options.state.x + options.drawLength * cos(radians(options.state.currentAngle));
-    let y1 = options.state.y + options.drawLength * sin(radians(options.state.currentAngle));
+    let drawLength = options.drawLength[options.iteration];
+    let x1 = options.state.x + drawLength * cos(radians(options.state.currentAngle));
+    let y1 = options.state.y + drawLength * sin(radians(options.state.currentAngle));
     line(options.state.x, options.state.y, x1, y1); // connect the old and the new
 
     // update the turtle's position:
